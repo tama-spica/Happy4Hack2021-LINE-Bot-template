6ee2ce13a95f46a8aa9ca2b57ae280efc7b83648
tama <kojima.tamaki.2017@gmail.com>
2021-10-31 12:12:39 +0000

    test

--- deployPrj01/LineHttpTriggeredFunction/index.js
+++ deployPrj01/LineHttpTriggeredFunction/index.js
@@ -65,7 +65,7 @@ async function handleEvent(event) {
       //https://developers.line.biz/ja/reference/messaging-api/#quick-reply
       return client.replyMessage(event.replyToken,{
         type: 'text',
-        text: 'ステッカー欲しいですか❓YesかNoで答えてください, もしくは素敵な写真送って❗️',
+        text: 'ステッカー欲しいですかYesかNoで答えてください, もしくは素敵な写真送って❗️',
         "quickReply": {
           "items": [
             {
