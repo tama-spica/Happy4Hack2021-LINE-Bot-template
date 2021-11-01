f2299023595685fb2980f8ad82f678cf6f3ac404
tama <kojima.tamaki.2017@gmail.com>
2021-11-01 00:05:37 +0000

    commit

--- deployPrj01/LineHttpTriggeredFunction/index.js
+++ deployPrj01/LineHttpTriggeredFunction/index.js
@@ -37,6 +37,29 @@ app.post('/api/linehttptriggeredfunction', line.middleware(config), (req, res) =
     });
 });
 
+//https://developers.line.biz/ja/reference/messaging-api/#send-push-message
+const line = require('@line/bot-sdk');
+
+const token = '0SNPpsEPdri95On5F3ZZruJcObqwI+3UGADGZn4IVX96ZdUHcHZq1HCVGEgtaiSzrqhX0SB5GgGXnDRLdE8Rv/oYNzQVtbcFnSbda4xCsd296yzr3TNDrmKAGXZzziFqARfTSf1WlQvQABTwBYHDvwdB04t89/1O/w1cDnyilFU='
+const userId = 'U568a9510055a2c90105cd5eff2868a78'
+
+const client = new line.Client({
+  channelAccessToken: token
+});
+
+const message = {
+  type: 'text',
+  text: 'Hello World!'
+};
+
+client.pushMessage(userId, message)
+  .then(() => {
+    console.log('push!')
+  })
+  .catch((err) => {
+    // error handling
+});
+
 // event handler
 async function handleEvent(event) {
   if (event.type !== 'message' && event.type !== 'postback') {
